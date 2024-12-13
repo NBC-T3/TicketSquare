09602a01d90442a76a377d137b7ddf3ae886078c
이재영 <ldc1000@naver.com>
2024-12-13 21:17:48 +0900

    🎉[Init] : 프로젝트 시작

---
+++ TicketSquare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TicketSquare
+//
+//  Created by 이재건 on 12/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
